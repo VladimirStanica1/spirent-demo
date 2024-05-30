98df3923bfe21ece886b954774d2cec04a769120
ALEXANDRU.STANICA <alexandru.chisbora_ext@haiilo.com>
2024-05-31 02:32:37 +0300

    check commit

--- target/classes/db/changelog/v001_bird.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE bird
-(
-    id     BIGINT PRIMARY KEY AUTO_INCREMENT,
-    name   VARCHAR(255) NOT NULL,
-    color  VARCHAR(255),
-    weight DECIMAL(10, 2),
-    height DECIMAL(10, 2)
-);
-
--- target/classes/db/changelog/v003_provisioning_bird.sql
+++
@@ -1,6 +0,0 @@
--- Insert birds
-INSERT INTO bird (name, color, weight, height)
-VALUES
-    ('Sparrow', 'Brown', 0.05, 0.1),
-    ('Robin', 'Red', 0.07, 0.15),
-    ('Blue Jay', 'Blue', 0.08, 0.12);
